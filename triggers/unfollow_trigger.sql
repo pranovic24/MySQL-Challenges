ca3eaabed49073ffba829b7cc994d92362189c39
pranaysood24 <pranay.sd@gmail.com>
2022-08-29 16:50:30 +0000

    create unfollow_triggers

---
+++ triggers/unfollow_trigger.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE TRIGGER capture_unfollow
+     AFTER DELETE ON follows FOR EACH ROW
+     BEGIN
+     	# INSERT INTO unfollows(follower_id, followee_id)
+        # VALUES(OLD.follower_id, OLD.followee_id);
+        INSERT INTO unfollows
+        SET 
+        follower_id = OLD.follower_id,
+        followee_id = OLD.followee_id;
+     END;
+$$
+
+DELIMITER ;
